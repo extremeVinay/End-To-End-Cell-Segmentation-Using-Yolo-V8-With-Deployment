3332316f567b3bf251799762f96858271d965452
Vinay <learningfrombirth@gmail.com>
2023-08-04 09:59:46 +0530

    dockerfile added

--- cellSegmentation/utils/main_utils.py
+++ cellSegmentation/utils/main_utils.py
@@ -47,3 +47,6 @@ def decodeImage(imgstring, fileName):
 def encodeImageIntoBase64(croppedImagePath):
     with open(croppedImagePath, "rb") as f:
         return base64.b64encode(f.read())
+
+    
+    
